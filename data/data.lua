70ff0fba75b44e8b2b45153ec2c6c99cea70d824
Agimir <william@mesaindigo.com>
2024-07-28 19:35:27 -0500

    yur

---
+++ data/data.lua
@@ -0,0 +1,37 @@
+Data.Blip = { spr = 475, scl = 0.6, clr = 26, lbl = 'Go Postal Office'}
+
+Data.Ped = { model = 's_m_m_postal_01', scenario = 'WORLD_HUMAN_CLIPBOARD_FACILITY'}
+
+Data.Location = vec4(133.162, 96.645, 83.507, 159.803)
+
+Data.Vehicle = {
+    model = 'nspeedo', livery = 4,
+    spawn = vec4(117.250, 99.163, 80.735, 70.156),
+    despawn = {
+        size = vec3(5.0, 8.0, 5.0), rotation = 340.0,
+    }
+}
+
+Data.Uniform = {
+    male = {
+
+    },
+    female = {
+
+    },
+}
+
+Data.Items = {
+    ob1 = { item = '', count = 1, meta = 'Prop. of _insert_' },
+    ob2 = { item = '', count = 1, meta = 'Prop. of _insert_' },
+    ob3 = { item = '', count = 1, meta = 'Prop. of _insert_' },
+}
+
+Data.Tasks = {
+    t1 = {
+        label = '',
+        zone = {
+
+        }
+    },
+}
